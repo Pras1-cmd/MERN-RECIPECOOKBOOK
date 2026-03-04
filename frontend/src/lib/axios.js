d24d257e1d5b6e14986fb12791285fb4f4b15c1d
Pras1-cmd <chmcs2026.prasenjit.singh@gmail.com>
2026-03-04 15:38:55 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mern-recipecookbook.onrender.com",
+  baseURL: "https://mern-recipecookbook.onrender.com/",
 });
 
 export default API;
